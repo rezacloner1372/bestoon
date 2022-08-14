1a3ae06de450da26c97b809fd095dda3aef8d95c
rezacloner1372 <rezabermuda72@yahoo.com>
2022-08-14 16:47:55 +0430

    expenese model is there

--- web/models.py
+++ web/models.py
@@ -1,4 +1,13 @@
+from cgitb import text
 from django.db import models
 
 #from __future__ import unicode_literals
 # Create your models here.
+from django.contrib.auth.models import User
+
+
+class Expense(models.Model):
+    text = models.CharField(max_length=255)
+    amount = models.BigIntegerField()
+    date = models.DateTimeField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
