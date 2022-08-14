9a42537a5d317534af465c00c9dc104e0e9f2cfb
rezacloner1372 <rezabermuda72@yahoo.com>
2022-08-14 17:29:25 +0430

    gitignore and requirements

--- web/models.py
+++ web/models.py
@@ -12,9 +12,15 @@ class Expense(models.Model):
     date = models.DateTimeField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return "{} - {}".format(self.date, self.amount)
+
 
 class Income(models.Model):
     text = models.CharField(max_length=255)
     amount = models.BigIntegerField()
     date = models.DateTimeField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "{} - {}".format(self.date, self.amount)
