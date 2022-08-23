58b88226a73cddc114530e4bd4ee7b45a4670235
rezacloner1372 <rezabermuda72@yahoo.com>
2022-08-23 19:25:18 +0430

    income

--- web/views.py
+++ web/views.py
@@ -10,6 +10,21 @@ from datetime import date, datetime
 
 
 @csrf_exempt
+def submit_income(request):
+
+    this_token = request.POST.get('token')
+    this_user = User.objects.filter(token__token=this_token).get()
+    if 'date' not in request.POST:
+        date = datetime.now()
+
+    Income.objects.create(text=request.POST.get(
+        'text'), amount=request.POST.get('amount'), date=date, user=this_user)
+
+    return JsonResponse({'status': 'ok'},
+                        encoder=JSONEncoder)
+
+
+@csrf_exempt
 def submit_expense(request):
 
     this_token = request.POST.get('token')
